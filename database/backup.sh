477265842680cc06fd0563035d80bc0bacec99e2
Julien Klopfer <julien.klopfer@gmail.com>
2017-04-28 11:31:46 -0400

    Update database backup script

--- database/backup.sh
+++ database/backup.sh
@@ -1,13 +1,3 @@
-#chttps://dev.mysql.com/doc/refman/5.7/en/mysqldump.html
+#!/bin/sh
 
-#https://www.digitalocean.com/community/tutorials/how-to-import-and-export-databases-and-reset-a-root-password-in-mysql
-#https://www.digitalocean.com/community/tutorials/how-to-backup-mysql-databases-on-an-ubuntu-vps
-
-#http://askubuntu.com/questions/698845/automatic-backup-of-mysql-on-ubuntu
-
-#http://stackoverflow.com/questions/9293042/how-to-perform-a-mysqldump-without-a-password-prompt?answertab=votes#tab-top
-
-mysqldump -u ulli -p Sherwood109 Images | gzip > ./images_backup_`date +%F`.sql.gz
-
-
-/usr/local/mysql/bin/mysqldump -u ulli -pSherwood109 Images | gzip > /Users/julien/Documents/Dev/Connexions/cnx-images/database/images_backup_`date +%F`.sql.gz
+mysqldump -u ulli Images | gzip > /run/user/1000/gvfs/smb-share:server=sol-file,share=samba/Backups/database/images_backup_`date +%F-%s`.sql.gz
