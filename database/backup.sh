2ebe4c18dc90cd35a6fc66beb31414f454f75cdf
Julien Klopfer <julien.klopfer@gmail.com>
2017-05-02 13:27:03 -0400

    Delete backups older than 30 days

--- database/backup.sh
+++ database/backup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+# Dump the Images databse and copy the file to the Samba share on the SOL machine
+
 mysqldump -u ulli Images | gzip > /run/user/1000/gvfs/smb-share:server=sol-file,share=samba/Backups/database/images_backup_`date +%F-%s`.sql.gz
+
+# Delete backups older than 30 days
+
+find /run/user/1000/gvfs/smb-share:server=sol-file,share=samba/Backups/database -mtime +30 -type f -delete
+
+# To restore a backup use the below command:
+
+# mysql -u ulli Images < backup_to_restore.sql
