ae161e02b1d17bf95684f3bcfe5d33215dcae678
Julien Klopfer <julien.klopfer@gmail.com>
2017-05-05 23:41:52 -0400

    Add Bootstrap scripts + Remove unused CSS

--- public/includes/styles.inc.php
+++ public/includes/styles.inc.php
@@ -1,16 +1,11 @@
-<link href="styles/jquery-ui.min.css" rel="stylesheet">
-<!-- <link href="styles/style.css" rel="stylesheet"> -->
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" href="styles/jquery-ui.min.css">
+<link rel="stylesheet" href="styles/bootstrap.min.css">
+<link rel="stylesheet" href="styles/bootstrap-theme.min.css">
 
 <style>
 	.page-size {
 	    width:auto;
 	    display:inline-block;
+		margin: 18px 0;
 	}
 </style>
-
